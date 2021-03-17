6a87f36bf607cddb4963f48c891d81aa8399602a
WHITE-GRINT\Kazuki <kzktcy0610@gmail.com>
2021-03-17 21:47:11 +0900

    さらに修正

--- 4.py
+++ 4.py
@@ -1,14 +1,12 @@
+import re
+
 string = 'Now I need a drink, alcoholic of course, after the heavy lectures involving quantum mechanics.'
+string = re.sub('[,\.]', '', string) 
 
 string = string.split()
 length_list = []
 
 for str in string:
-    if ',' in str:        
-        length_list.append(len(str) - str.count(','))
-    elif '.' in str:
-        length_list.append(len(str) - str.count('.'))
-    else:
         length_list.append(len(str))
 
 print(length_list)
